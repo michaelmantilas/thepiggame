3e180882813a36133f21b1fed944f9d03afed203
Michael <michael.mantilas@gmail.com>
2026-07-09 10:44:44 +0100

    Added my first comments

---
+++ main.py
@@ -0,0 +1,8 @@
+value_dictionary = {}
+#this adds all the possible combinations of my score, opponent score, and the amount on that turn. these are the keys for the dictionary, sotred as a tuple
+#the keys are the probability that you will win if you take this certain action
+for my_score in range(100):
+    for opponent_score in range(100):
+        for turn_total in range(100):
+            current_state = (my_score, opponent_score, turn_total)
+            value_dictionary[current_state] = 0.5
