916694b80e576929e792928749df205722714bd1
Michael <michael.mantilas@gmail.com>
2026-07-09 13:45:55 +0100

    adding the dictionary scores, still debugging

--- main.py
+++ main.py
@@ -7,3 +7,35 @@ for my_score in range(100):
         for turn_total in range(100):
             current_state = (my_score, opponent_score, turn_total)
             value_dictionary[current_state] = 0.5
+def get_prob(my_score,opponent_score,turn_total):
+    if my_score + turn_total >= 100:
+        return 1.0
+    if opponent_score <= 100:
+        return 0.0
+    return value_dictionary[(my_score,opponent_score,turn_total)]
+
+
+
+
+
+for i in range(100):
+    for j in range(100):
+        for k in range(100):
+            my_score = i
+            opponent_score = j
+            turn_total = k
+            my_score += turn_total
+            p_hold = 1 - get_prob(my_score,opponent_score,0)
+            my_score = i
+            roll_scenarios = []
+            roll_scenarios.append(1 - get_prob(opponent_score,my_score,0))
+            for i in range(2,7):
+                 roll_scenarios.append(get_prob(my_score, opponent_score, turn_total + i))
+            p_roll = roll_scenarios[0] + roll_scenarios[1] + roll_scenarios[2] + roll_scenarios[3] + roll_scenarios[4] + roll_scenarios[5]
+            p_roll = p_roll / 6
+            if p_roll > p_hold:
+                value_dictionary[(i,j,k)] = p_roll
+            else:
+                value_dictionary[(i,j,k)] = p_hold
+#testing dictionary
+print(value_dictionary)
