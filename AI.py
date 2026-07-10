1e20953bc84ba40517fb9d94669e1d7947e2f602
Michael <michael.mantilas@gmail.com>
2026-07-10 10:04:53 +0100

    Making the two files dependent on each other

--- AI.py
+++ AI.py
@@ -1,11 +1,14 @@
+from main import get_prob
 
 def make_ai_decision(my_score, opponent_score, turn_total):
     #if I stop, what is my probability to win?
-    chance_if_hold = get_win_probability(my_score + turn_total, opponent_score, 0)
+    chance_if_hold = get_prob(my_score + turn_total, opponent_score, 0)
     #if I roll, what is my chance to win and my chance to lose all points on this turn?
-    chance_if_roll = calculate_roll_chance(my_score, opponent_score, turn_total)
+    chance_if_roll = get_prob(my_score, opponent_score, turn_total)
     #compares both probabilities, bigger is better
     if chance_if_hold > chance_if_roll:
-        return "HOLD"
+        return "Hold"
     else:
-        return "ROLL"
+        return "Roll"
+
+print(make_ai_decision(67,89,22))
--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 
 value_dictionary = {}
-#this adds all the possible combinations of my score, opponent score, and the amount on that turn. these are the keys for the dictionary, sotred as a tuple
-#the keys are the probability that you will win if you take this certain action
-for my_score in range(100):
-    for opponent_score in range(100):
-        for turn_total in range(100):
-            current_state = (my_score, opponent_score, turn_total)
-            value_dictionary[current_state] = 0.5
 def get_prob(my_score,opponent_score,turn_total):
     if my_score + turn_total >= 100:
         return 1.0
     if opponent_score >= 100:
         return 0.0
     return value_dictionary[(my_score,opponent_score,turn_total)]
-
-
-
-
+#this adds all the possible combinations of my score, opponent score, and the amount on that turn. these are the keys for the dictionary, stored as a tuple
+#the keys are the probability that you will win if you take this certain action
+for my_score in range(100):
+    for opponent_score in range(100):
+        for turn_total in range(100):
+            current_state = (my_score, opponent_score, turn_total)
+            value_dictionary[current_state] = 0.5
 
 for i in range(100):
     for j in range(100):
@@ -37,5 +33,3 @@ for i in range(100):
                 value_dictionary[(i,j,k)] = p_roll
             else:
                 value_dictionary[(i,j,k)] = p_hold
-#testing dictionary
-print(value_dictionary)
