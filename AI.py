1b4bf0ee70939c2a9908fe4c3bfffdc13f3966fb
Michael <michael.mantilas@gmail.com>
2026-07-10 11:25:18 +0100

    cannot run in two files, only one merged file

--- AI.py
+++ AI.py
@@ -1,5 +1,3 @@
-from main import get_prob
-
 def make_ai_decision(my_score, opponent_score, turn_total):
     #if I stop, what is my probability to win?
     chance_if_hold = get_prob(my_score + turn_total, opponent_score, 0)
@@ -10,5 +8,21 @@ def make_ai_decision(my_score, opponent_score, turn_total):
         return "Hold"
     else:
         return "Roll"
+#a test
 
-print(make_ai_decision(67,89,22))
+value_dictionary = {}
+
+
+def get_prob(my_score, opponent_score, turn_total):
+    if my_score + turn_total >= 100:
+        return 1.0
+    if opponent_score >= 100:
+        return 0.0
+    return value_dictionary[(my_score, opponent_score, turn_total)]
+
+
+# this adds all the possible combinations of my score, opponent score, and the amount on that turn. these are the keys for the dictionary, stored as a tuple
+# the keys are the probability that you will win if you take this certain action
+for my_score in range(100):
+    for opponent_score in range(1
+    print(make_ai_decision(67,89,22))
--- main.py
+++ main.py
@@ -6,6 +6,18 @@ def get_prob(my_score,opponent_score,turn_total):
     if opponent_score >= 100:
         return 0.0
     return value_dictionary[(my_score,opponent_score,turn_total)]
+
+def make_ai_decision(my_score, opponent_score, turn_total):
+    #if I stop, what is my probability to win?
+    chance_if_hold = get_prob(my_score + turn_total, opponent_score, 0)
+    #if I roll, what is my chance to win and my chance to lose all points on this turn?
+    chance_if_roll = get_prob(my_score, opponent_score, turn_total)
+    #compares both probabilities, bigger is better
+    if chance_if_hold > chance_if_roll:
+        return "Hold"
+    else:
+        return "Roll"
+
 #this adds all the possible combinations of my score, opponent score, and the amount on that turn. these are the keys for the dictionary, stored as a tuple
 #the keys are the probability that you will win if you take this certain action
 for my_score in range(100):
@@ -33,3 +45,4 @@ for i in range(100):
                 value_dictionary[(i,j,k)] = p_roll
             else:
                 value_dictionary[(i,j,k)] = p_hold
+
